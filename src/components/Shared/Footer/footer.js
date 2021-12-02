32209992385e3b4abb87b0bcfa635ad034e791d9
Prathmesh-Dhatrak <prathmesh101dhatrak@gmail.com>
2021-12-02 14:10:43 +0530

    new

--- src/components/Shared/Footer/footer.js
+++ src/components/Shared/Footer/footer.js
@@ -1,73 +1,76 @@
 import React from "react";
 // import { Link } from "react-router-dom";
 import "./footer.css";
+import facebook from "../../../assets/icon/facebook.svg";
+import youtube from "../../../assets/icon/youtube.svg";
+import instagram from "../../../assets/icon/instagram.svg";
 
 const Footer = () => {
   return (
-    <footer class="footer-section">
-      <div class="container">
-        <div class="footer-cta pt-5 pb-5">
-          <div class="row">
-            <div class="col-xl-4 col-md-4 mb-30">
-              <div class="single-cta">
-                <i class="fas fa-map-marker-alt"></i>
-                <div class="cta-text">
+    <footer className="footer-section">
+      <div className="container">
+        <div className="pt-5 pb-5 footer-cta">
+          <div className="row">
+            <div className="col-xl-4 col-md-4 mb-30">
+              <div className="single-cta">
+                <i className="fas fa-map-marker-alt"></i>
+                <div className="cta-text">
                   <h4>Find us</h4>
-                  <span>1010 Avenue, sw 54321, chandigarh</span>
+                  <span>1010 Lorem ipsum, sw 54321, Sit amet,</span>
                 </div>
               </div>
             </div>
-            <div class="col-xl-4 col-md-4 mb-30">
-              <div class="single-cta">
-                <i class="fas fa-phone"></i>
-                <div class="cta-text">
+            <div className="col-xl-4 col-md-4 mb-30">
+              <div className="single-cta">
+                <i className="fas fa-phone"></i>
+                <div className="cta-text">
                   <h4>Call us</h4>
-                  <span>9876543210 0</span>
+                  <span>9857899965</span>
                 </div>
               </div>
             </div>
-            <div class="col-xl-4 col-md-4 mb-30">
-              <div class="single-cta">
-                <i class="far fa-envelope-open"></i>
-                <div class="cta-text">
+            <div className="col-xl-4 col-md-4 mb-30">
+              <div className="single-cta">
+                <i className="far fa-envelope-open"></i>
+                <div className="cta-text">
                   <h4>Mail us</h4>
-                  <span>mail@info.com</span>
+                  <span>mail@xyz.com</span>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div class="footer-content pt-5 pb-5">
-          <div class="row">
-            <div class="col-xl-4 col-lg-4 mb-50">
-              <div class="footer-widget">
-                <div class="footer-widget-heading">
+        <div className="pt-5 pb-5 footer-content">
+          <div className="row">
+            <div className="col-xl-4 col-lg-4 mb-50">
+              <div className="footer-widget">
+                <div className="footer-widget-heading">
                   <h3>INDOSCULP</h3>
                 </div>
-                <div class="footer-text">
+                <div className="footer-text">
                   <p>
                     Lorem ipsum dolor sit amet, consec tetur adipisicing elit,
                     sed do eiusmod tempor incididuntut consec tetur adipisicing
                     elit,Lorem ipsum dolor sit amet.
                   </p>
                 </div>
-                <div class="footer-social-icon">
+                <div className="footer-social-icon">
                   <span>Follow us</span>
                   <a href=".">
-                    <i class="fab fa-facebook-f facebook-bg"></i>
+                    <img src={facebook} alt="My Happy SVG" />
                   </a>
                   <a href=".">
-                    <i class="fab fa-twitter twitter-bg"></i>
+                    <img src={youtube} alt="My Happy SVG" />
                   </a>
                   <a href=".">
-                    <i class="fab fa-google-plus-g google-bg"></i>
+                    <img src={instagram} alt="My Happy SVG" />
                   </a>
                 </div>
               </div>
             </div>
-            <div class="col-xl-4 col-lg-4 col-md-6 mb-30">
-              <div class="footer-widget">
-                <div class="footer-widget-heading">
+            <div className="col-xl-4 col-lg-4 col-md-6 mb-30">
+              <div className="footer-widget">
+                <div className="footer-widget-heading">
                   <h3>Useful Links</h3>
                 </div>
                 <ul>
@@ -104,22 +107,22 @@ const Footer = () => {
                 </ul>
               </div>
             </div>
-            <div class="col-xl-4 col-lg-4 col-md-6 mb-50">
-              <div class="footer-widget">
-                <div class="footer-widget-heading">
+            <div className="col-xl-4 col-lg-4 col-md-6 mb-50">
+              <div className="footer-widget">
+                <div className="footer-widget-heading">
                   <h3>Subscribe</h3>
                 </div>
-                <div class="footer-text mb-25">
+                <div className="footer-text mb-25">
                   <p>
                     Don’t miss to subscribe to our new feeds, kindly fill the
                     form below.
                   </p>
                 </div>
-                <div class="subscribe-form">
+                <div className="subscribe-form">
                   <form action="#">
                     <input type="text" placeholder="Email Address" />
                     <button>
-                      <i class="fab fa-telegram-plane"></i>
+                      <i className="fab fa-telegram-plane"></i>
                     </button>
                   </form>
                 </div>
@@ -128,19 +131,19 @@ const Footer = () => {
           </div>
         </div>
       </div>
-      <div class="copyright-area">
-        <div class="container">
-          <div class="row">
-            <div class="col-xl-6 col-lg-6 text-center text-lg-left">
-              <div class="copyright-text">
+      <div className="copyright-area">
+        <div className="container">
+          <div className="row">
+            <div className="text-center col-xl-6 col-lg-6 text-lg-left">
+              <div className="copyright-text">
                 <p>
                   Copyright &copy; 2020, All Right Reserved{" "}
                   <a href="https://www.indosculp.com/">Indosculp</a>
                 </p>
               </div>
             </div>
-            <div class="col-xl-6 col-lg-6 d-none d-lg-block text-right">
-              <div class="footer-menu">
+            <div className="text-right col-xl-6 col-lg-6 d-none d-lg-block">
+              <div className="footer-menu">
                 <ul>
                   <li>
                     <a href=".">Home</a>
