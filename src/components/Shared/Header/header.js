ede686bda60702124759df7c47831b215c7ce36c
shelkepranav <pranavshelke4@gmail.com>
2021-12-18 14:07:54 +0530

    Header Done

--- src/components/Shared/Header/header.js
+++ src/components/Shared/Header/header.js
@@ -1,12 +1,14 @@
 import React from "react";
-import { Navbar, Nav, NavDropdown } from "react-bootstrap";
+import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap";
 import "./header.css";
-import logo from "../../../assets/img/LOGO-01.png";
+import logo from "../../../assets/img/Logo1.svg";
+
+
 
 const Header = () => {
   return (
     <>
-      <Navbar bg="light" variant="light" expand="md" fixed="top">
+      {/* <Navbar bg="light" variant="light" expand="md" fixed="top">
         <Navbar.Brand href="/" className="px-4">
           <img alt="" src={logo} />
         </Navbar.Brand>
@@ -87,7 +89,45 @@ const Header = () => {
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
+      </Navbar> */}
+
+
+      <Navbar collapseOnSelect expand="lg" fixed="top">
+          <Navbar.Brand href="#home"><img alt="" src={logo} /><b>Indosculp</b></Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link className="nav-link" href="#features"><b>Products</b></Nav.Link>
+              <NavDropdown className="nav-dropdown" title={<b>Markets & Applications</b>} id="collasible-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Agriculture</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Construction</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Food & Beverage</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.4">Intralogistics</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Logistics</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.4">Marine & Offshore</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Mining</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.4">Packaging</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Wind</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.4">Textile</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.4">Recycling</NavDropdown.Item>
+              </NavDropdown>
+              <NavDropdown title={<b>Company</b>} id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">About us</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Excellence</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Financial Highlights</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.4">Social Responsibility</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">News, Media & Events</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.4">Contacts</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Bonfiglioli Service</NavDropdown.Item>
+              </NavDropdown>
+              <Nav.Link href="#pricing"><b>Peoples</b></Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link href="#deets"><i class='bx bxl-facebook bx-md'></i></Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
       </Navbar>
+
     </>
   );
 };
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,112 +1,12 @@
 import "./App.css";
-import { Container, Row, Col, Card, Button } from "react-bootstrap";
-import { useState } from "react";
-// import { Link } from 'react-router-dom';
-import { Carousel } from "react-bootstrap";
-import logo from "../assets/img/LOGO-01.png";
-import product from "../assets/img/Products/product.png";
-import banner from "../assets/img/Products/banner2.jpg";
+import Slideshow from "../components/Shared/Slider/Slider";
+import Product from "../components/Shared/Product/Product";
 
 function App() {
-  const [index, setIndex] = useState(0);
-
-  const handleSelect = (selectedIndex, e) => {
-    setIndex(selectedIndex);
-  };
   return (
     <div className="App">
-      <Container>
-        <Row>
-          <Col>
-            <Carousel
-              activeIndex={index}
-              onSelect={handleSelect}
-              variant="dark"
-            >
-              <Carousel.Item>
-                <img
-                  className="d-flex img-fluid"
-                  src={product}
-                  alt="First slide"
-                />
-                <Carousel.Caption className="text-light">
-                  <h3>Product Name</h3>
-                  <p>
-                    Nulla vitae elit libero, a pharetra augue mollis interdum.
-                  </p>
-                </Carousel.Caption>
-              </Carousel.Item>
-              <Carousel.Item>
-                <img
-                  className="d-flex img-fluid"
-                  src={logo}
-                  alt="Second slide"
-                />
-                {/* <Carousel.Caption className="text-light">
-                  <h3>Product Name</h3>
-                  <p>
-                    Nulla vitae elit libero, a pharetra augue mollis interdum.
-                  </p>
-                </Carousel.Caption> */}
-              </Carousel.Item>
-              <Carousel.Item>
-                <img
-                  className="d-flex img-fluid"
-                  src={banner}
-                  alt="Third slide"
-                />
-                {/* <Carousel.Caption className="text-light">
-                  <h3>Product Name</h3>
-                  <p>
-                    Nulla vitae elit libero, a pharetra augue mollis interdum.
-                  </p>
-                </Carousel.Caption> */}
-              </Carousel.Item>
-            </Carousel>
-          </Col>
-        </Row>
-        <div className="container my-4 card-deck row">
-          <div className="col-xs-12 col-sm-6 col-md-4 ">
-            <Card>
-              <Card.Img variant="top" src={product} />
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-              </Card.Body>
-            </Card>
-          </div>
-          <div className="col-xs-12 col-sm-6 col-md-4">
-            <Card>
-              <Card.Img variant="top" src={product} />
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-              </Card.Body>
-            </Card>
-          </div>
-          <div className="col-xs-12 col-sm-6 col-md-4">
-            <Card>
-              <Card.Img variant="top" src={product} />
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-              </Card.Body>
-            </Card>
-          </div>
-        </div>
-      </Container>
+      <Slideshow />
+      <Product />
     </div>
   );
 }
