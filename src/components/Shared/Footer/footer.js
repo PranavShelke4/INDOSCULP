5bfb9ad19d9bd26de62492d64001de99f5d03615
shelkepranav <pranavshelke4@gmail.com>
2021-12-25 14:07:58 +0530

    home page done

--- src/components/Shared/Footer/footer.js
+++ src/components/Shared/Footer/footer.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./footer.css";
 
-import logo from "../../../assets/img/Logo1.svg";
+import logo from "../../../assets/icon/LOGO_new.png";
 import facebook from "../../../assets/icon/facebook.svg";
 import youtube from "../../../assets/icon/youtube.svg";
 import instagram from "../../../assets/icon/instagram.svg";
@@ -12,11 +12,10 @@ const Footer = () => {
       <footer>
         <div className="about col-lg-4 col-md-12 col-sm-12">
             <img alt="" className="logo" src={logo} />
-            <span><b>Indosculp</b></span>
 
-            <p>As per the demands of the clients, we provide our range of engineering components in various specifications. 
-              Our packaging experts effectively package these items effectively on the basis of their requirements. 
-              Due to these reasons, we have achieved a large customer base in India.</p>
+            <p>Incepted in the year 2007, Indosculp Auto comp Pvt. Ltd is one of the leading manufacturers and suppliers 
+              of an assortment of various engineering components such as finish Auto-gear components, Transmission Gears, 
+              API couplings, Electric motor shafts, Worm gears, cluster gears, connecting shafts.</p>
             
             <a href="."><img className="social" src={facebook} alt="facebook" /></a>
             <a href="."><img className="social" src={youtube} alt="youtube" /></a>
@@ -25,6 +24,7 @@ const Footer = () => {
             <p className="Copyright">
               Copyright &copy; 2020, All Right Reserved{" "}
               <a href="https://www.indosculp.com/">Indosculp</a>
+              <br/>Made with <a href="https://webeeyo.com/">webeeyo</a>
             </p>
         </div>
 
@@ -35,10 +35,10 @@ const Footer = () => {
             <div className="Our_Products col-lg-4 col-md-12 col-sm-12">
             <div><h4>Our Products</h4></div>
               <ul>
-                <li>Three Wheeler Gears</li>
-                <li>Electical Motor Shaft</li>
-                <li>API COUPLINGS</li>
-                <li>Compressor Block Wheeler Gears</li>
+                <li><a className="pages" href="/">PS25 Foot Type</a></li>
+                <li><a className="pages" href="/">PS25 Flange Type</a></li>
+                <li><a className="pages" href="/">PS30/35 Foot Type</a></li>
+                <li><a className="pages" href="/">PS30/35 Flange Type</a></li>
               </ul>
             </div>
 
@@ -50,25 +50,23 @@ const Footer = () => {
                 <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                 <circle cx="12" cy="11" r="3" />
                 <path d="M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z" />
-              </svg>MIDC Area-Vallabhnagar,Ahmednagar 400100, Maharastra.<br/>
+              </svg>A-67, M.I.D.C. Ahmednagar, <br/>Mahrashtra INDIA - 414111<br/>
 
               <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-mail" width="20" height="20" viewBox="0 0 24 24" stroke-width="1" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
                 <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                 <rect x="3" y="5" width="18" height="14" rx="2" />
                 <polyline points="3 7 12 13 21 7" />
-              </svg>Indosculp@gmail.com <br/>
+              </svg>indosculp@gmail.com<br/>
 
               <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-phone" width="20" height="20" viewBox="0 0 24 24" stroke-width="1" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
                 <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                 <path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2" />
-              </svg>0123556677<br/>
+              </svg>+91 0241 2778844<br/>
 
               <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-phone" width="20" height="20" viewBox="0 0 24 24" stroke-width="1" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
                 <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                 <path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2" />
-              </svg>0123556677<br/>
-
-              
+              </svg>+91 0241 2779584<br/>
 
             </div>
 
@@ -76,10 +74,10 @@ const Footer = () => {
             <div className="Quick_Links col-lg-4 col-md-12 col-sm-12">
               <h4>Quick Links</h4>
               <ul>
-                <li><a className="pages" href="/">About</a></li>
-                <li><a className="pages" href="/">Companies</a> </li>
-                <li><a className="pages" href="/">Industries</a> </li>
-                <li><a className="pages" href="/">Product</a></li>
+                <li><a className="pages" href="/">About Us</a></li>
+                <li><a className="pages" href="/">Contact Us</a> </li>
+                <li><a className="pages" href="/">Products</a> </li>
+                <li><a className="pages" href="/">Peoples</a></li>
               </ul>
             </div>
 
