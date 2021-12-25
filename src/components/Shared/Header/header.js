4c8823821dc7720a77db3bed5fb65f73776caf12
shelkepranav <pranavshelke4@gmail.com>
2021-12-25 21:17:54 +0530

    Agriculture page done

--- src/components/Shared/Header/header.js
+++ src/components/Shared/Header/header.js
@@ -14,7 +14,7 @@ const Header = () => {
     if (prevScrollpos > currentScrollPos) {
       document.getElementById("navbar").style.top = "0";
     } else {
-      document.getElementById("navbar").style.top = "-90px";
+      document.getElementById("navbar").style.top = "-350px";
     }
     prevScrollpos = currentScrollPos;
   }
