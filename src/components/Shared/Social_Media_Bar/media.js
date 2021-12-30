0aac9bfc32e4a3bfe97277b66a4093b9876a1429
Prathmesh-Dhatrak <prathmesh101dhatrak@gmail.com>
2021-12-30 10:51:46 +0530

    gfff

--- src/components/Shared/Social_Media_Bar/media.js
+++ src/components/Shared/Social_Media_Bar/media.js
@@ -1,14 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "./media.css";
 const Media = () => {
-  return (
-    <div class="social-links">
+  const [arrow, setArrow] = useState(true);
+  const Social = () => (
+    <>
+      <div
+        class="social-btn flex-center"
+        id="arrow"
+        onClick={() => {
+          setArrow(!arrow);
+        }}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="icon icon-tabler icon-tabler-arrows-down"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="#ffffff"
+          fill="none"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <line x1="7" y1="21" x2="7" y2="3" />
+          <path d="M20 18l-3 3l-3 -3" />
+          <path d="M4 18l3 3l3 -3" />
+          <line x1="17" y1="21" x2="17" y2="3" />
+        </svg>
+      </div>
       <div class="social-btn flex-center" id="youtube">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 48 48"
-          width="48px"
-          height="48px"
+          width="35px"
+          height="35px"
         >
           <path
             fill="#FF3D00"
@@ -20,19 +47,18 @@ const Media = () => {
       <div class="social-btn flex-center" id="twitter">
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
+          width="20"
+          height="20"
           viewBox="0 0 24 24"
         >
           <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z" />
         </svg>
       </div>
-
       <div class="social-btn flex-center" id="linkedin">
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
+          width="20"
+          height="20"
           viewBox="0 0 24 24"
         >
           <path d="M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z" />
@@ -42,8 +68,8 @@ const Media = () => {
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 40 40"
-          width="30px"
-          height="30px"
+          width="28px"
+          height="28px"
         >
           <path
             fill="#8585cc"
@@ -112,14 +138,54 @@ const Media = () => {
       <div class="social-btn flex-center" id="facebook">
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
+          class="icon icon-tabler icon-tabler-brand-facebook"
+          width="28"
+          height="28"
           viewBox="0 0 24 24"
+          stroke-width="0.8"
+          stroke="#ffffff"
+          fill="none"
+          stroke-linecap="round"
+          stroke-linejoin="round"
         >
           <path stroke="none" d="M0 0h24v24H0z" fill="none" />
           <path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3" />
         </svg>
       </div>
+    </>
+  );
+  return (
+    <div class="social-links">
+      {arrow ? (
+        <Social />
+      ) : (
+        <div
+          class="social-btn flex-center"
+          id="arrow-up"
+          onClick={() => {
+            setArrow(!arrow);
+          }}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="icon icon-tabler icon-tabler-arrows-up"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="#ffffff"
+            fill="none"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <line x1="17" y1="3" x2="17" y2="21" />
+            <path d="M4 6l3 -3l3 3" />
+            <path d="M20 6l-3 -3l-3 3" />
+            <line x1="7" y1="3" x2="7" y2="21" />
+          </svg>
+        </div>
+      )}
     </div>
   );
 };
