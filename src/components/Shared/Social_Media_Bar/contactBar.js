8b8a8a6be652cdf3cdc5420cc0967d0ca9ca91be
Prathmesh-Dhatrak <prathmesh101dhatrak@gmail.com>
2021-12-30 14:11:12 +0530

    todays

--- src/components/Shared/Social_Media_Bar/contactBar.js
+++ src/components/Shared/Social_Media_Bar/contactBar.js
@@ -6,7 +6,22 @@ const ContactBar = () => {
       <div class="mail-btn flex-center" id="mail">
         <samp>CONTACT US</samp>
         <svg
-          fill="#000000"
+          xmlns="http://www.w3.org/2000/svg"
+          class="icon icon-tabler icon-tabler-border-vertical"
+          width="30"
+          height="55"
+          viewBox="0 0 24 24"
+          stroke-width="2"
+          stroke="#ffffff"
+          fill="none"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <line x1="12" y1="4" x2="12" y2="20" />
+        </svg>
+        <svg
+          fill="#ffffff"
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 512 512"
           width="36"
