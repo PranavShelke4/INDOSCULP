65ccc8c0428cb8ea839974d098672b0416ab17ef
Prathmesh-Dhatrak <prathmesh101dhatrak@gmail.com>
2021-12-30 16:11:55 +0530

    product

--- src/components/Shared/Product/Product.js
+++ src/components/Shared/Product/Product.js
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from "react";
 import { Container } from "react-bootstrap";
 import product from "../../../assets/img/Products/product.png";
-import './Product.css'
+import "./Product.css";
 
 function Product() {
-    return (
-        <div className='product'>
-        <Container>
+  return (
+    <div className="product">
+      <Container>
         {/* <div className="container my-4 card-deck row">
         
           <div className="col-xs-12 col-sm-6 col-md-4 cards">
@@ -54,67 +54,133 @@ function Product() {
               </a>
           </div>
         </div> */}
-
-        <div className='row' id='products'>
-
-              <div className="col-sm-12 col-md-4 col-lg-6" id='productCard'>
-                  <a href='/PS25'>
-                    <img className='productImg' src={product} alt='product1'/>
-                    <h3>PS25 Foot Type</h3>
-                    <p>Some quick example text to build on the card title and make up
-                      the bulk of the card's content.</p>
-                  </a>
+        <div className="d-flex flex-column flex-sm-column product">
+          <div className="d-flex flex-md-row flex-sm-column">
+            <a href="/PS25">
+              <div className="rounded" id="productCard">
+                <img className="productImg" src={product} alt="product1" />
+                <div className="p-2">
+                  <h3>PS25 Foot Type</h3>
+                  <p>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </p>
+                </div>
               </div>
-
-              <div className="col-sm-12 col-md-4 col-lg-6" id='productCard'>
-                  <a href='/'>
-                    <img className='productImg' src={product} alt='product1'/>
-                    <h3>PS25 Flange Type</h3>
-                    <p>Some quick example text to build on the card title and make up
-                      the bulk of the card's content.</p>
-                  </a>
+            </a>
+            <a href="/PS25">
+              <div className="rounded" id="productCard">
+                <img className="productImg" src={product} alt="product1" />
+                <div className="p-2">
+                  <h3>PS25 Foot Type</h3>
+                  <p>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </p>
+                </div>
               </div>
-
-              <div className="col-sm-12 col-md-4 col-lg-6" id='productCard'>
-                  <a href='/'>
-                    <img className='productImg' src={product} alt='product1'/>
-                    <h3>PS30 Foot Type</h3>
-                    <p>Some quick example text to build on the card title and make up
-                      the bulk of the card's content.</p>
-                  </a>
+            </a>
+          </div>
+          <div className="d-flex flex-md-row flex-sm-column">
+            <a href="/PS25">
+              <div className="rounded" id="productCard">
+                <img className="productImg" src={product} alt="product1" />
+                <div className="p-2">
+                  <h3>PS25 Foot Type</h3>
+                  <p>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </p>
+                </div>
               </div>
-
-              <div className="col-sm-12 col-md-4 col-lg-6" id='productCard'>
-                  <a href='/'>
-                    <img className='productImg' src={product} alt='product1'/>
-                    <h3>PS30 Flange Type</h3>
-                    <p>Some quick example text to build on the card title and make up
-                      the bulk of the card's content.</p>
-                  </a>
+            </a>
+            <a href="/PS25">
+              <div className="rounded" id="productCard">
+                <img className="productImg" src={product} alt="product1" />
+                <div className="p-2">
+                  <h3>PS25 Foot Type</h3>
+                  <p>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </p>
+                </div>
               </div>
-
-              <div className="col-sm-12 col-md-4 col-lg-6" id='productCard'>
-                  <a href='/'>
-                    <img className='productImg' src={product} alt='product1'/>
-                    <h3>PS35 Foot Type</h3>
-                    <p>Some quick example text to build on the card title and make up
-                      the bulk of the card's content.</p>
-                  </a>
-              </div>
-
-              <div className="col-sm-12 col-md-4 col-lg-6" id='productCard'>
-                  <a href='/'>
-                    <img className='productImg' src={product} alt='product1'/>
-                    <h3>PS35 Flange Type</h3>
-                    <p>Some quick example text to build on the card title and make up
-                      the bulk of the card's content.</p>
-                  </a>
-              </div>
+            </a>
+          </div>
         </div>
 
-        </Container>
-        </div>
-    )
+        {/* <div className="row" id="products">
+          <div
+            href="/PS25"
+            className="col-sm-12 col-md-4 col-lg-6"
+            id="productCard"
+          >
+            <img className="productImg" src={product} alt="product1" />
+            <h3>PS25 Foot Type</h3>
+            <p>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+          </div>
+
+          <div className="col-sm-12 col-md-4 col-lg-6" id="productCard">
+            <a href="/">
+              <img className="productImg" src={product} alt="product1" />
+              <h3>PS25 Flange Type</h3>
+              <p>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+            </a>
+          </div>
+
+          <div className="col-sm-12 col-md-4 col-lg-6" id="productCard">
+            <a href="/">
+              <img className="productImg" src={product} alt="product1" />
+              <h3>PS30 Foot Type</h3>
+              <p>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+            </a>
+          </div>
+
+          <div className="col-sm-12 col-md-4 col-lg-6" id="productCard">
+            <a href="/">
+              <img className="productImg" src={product} alt="product1" />
+              <h3>PS30 Flange Type</h3>
+              <p>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+            </a>
+          </div>
+
+          <div className="col-sm-12 col-md-4 col-lg-6" id="productCard">
+            <a href="/">
+              <img className="productImg" src={product} alt="product1" />
+              <h3>PS35 Foot Type</h3>
+              <p>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+            </a>
+          </div>
+
+          <div className="col-sm-12 col-md-4 col-lg-6" id="productCard">
+            <a href="/">
+              <img className="productImg" src={product} alt="product1" />
+              <h3>PS35 Flange Type</h3>
+              <p>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+            </a>
+          </div>
+        </div> */}
+      </Container>
+    </div>
+  );
 }
 
-export default Product
+export default Product;
