911474edba9f3cd490ac754cf163f6962b619b80
shelkepranav <pranavshelke4@gmail.com>
2022-01-29 19:36:01 +0530

    product page done

--- src/components/Shared/Product/Product.js
+++ src/components/Shared/Product/Product.js
@@ -1,165 +1,34 @@
 import React from "react";
 import { Container } from "react-bootstrap";
-import productimg from "../../../assets/img/Products/product.png";
+import { Link } from "react-router-dom";
 import "./Product.css";
-
-
-
-const productdata = [
-  {
-    id: "1",
-    productname: "PS25 Foot Type",
-    productinfo:
-      "Some quick example text to build on the card title and make up the bulk of the card's content.",
-    productimg: require("../../../assets/img/Products/product.png"),
-  },
-  {
-    id: "2",
-    productname: "PS25 Flange Type",
-    productinfo:
-      "Some quick example text to build on the card title and make up the bulk of the card's content.",
-    productimg: require("../../../assets/img/Products/product.png"),
-  },
-  {
-    id: "3",
-    productname: "PS30 Foot Type",
-    productinfo:
-      "Some quick example text to build on the card title and make up the bulk of the card's content.",
-    productimg: require("../../../assets/img/Products/product.png"),
-  },
-  {
-    id: "4",
-    productname: "PS30 Flange Type",
-    productinfo:
-      "Some quick example text to build on the card title and make up the bulk of the card's content.",
-    productimg: require("../../../assets/img/Products/product.png"),
-  },
-  {
-    id: "5",
-    productname: "PS35 Foot Type",
-    productinfo:
-      "Some quick example text to build on the card title and make up the bulk of the card's content.",
-    productimg: require("../../../assets/img/Products/product.png"),
-  },{
-    id: "6",
-    productname: "PS35 Flange Type",
-    productinfo:
-      "Some quick example text to build on the card title and make up the bulk of the card's content.",
-    productimg: require("../../../assets/img/Products/product.png"),
-  },
-];
+import productdata from "./ProductData";
 
 function Product() {
   return (
     <div className="product">
       <Container>
         <div className="d-flex flex-md-row flex-sm-column flex-wrap justify-content-center">
-
           {productdata.map((product) => {
+            var link = `/product/:` + product.id;
             return (
-              <a href="/PS25" key={product.id}>
-                <div className="rounded" id="productCard">
-                  <img className="productImg" alt="product_image" src={productimg} />
-                  <div className="p-2">
-                    <h3 id="product__name">{product.productname}</h3>
-                    <p>{product.productinfo}</p>
+              <>
+                <Link to={link} key={product.id}>
+                  <div className="rounded" id="productCard">
+                    <img
+                      className="productImg"
+                      alt="product_image"
+                      src={product.themnel}
+                    />
+                    <div className="p-2">
+                      <h3 id="product__name">{product.productname}</h3>
+                      <p>{product.productinfo}</p>
+                    </div>
                   </div>
-                </div>
-              </a>
+                </Link>
+              </>
             );
           })}
-
-          {/* {productdata.map(({ id, productname1, productinfo1 }) => (
-                <div className="p-2">
-                  <h3 key={id}>{productname1}</h3>
-                  <p>{productinfo1}</p>
-                </div>
-              ))} */}
-
-          {/* <a href="/PS25">
-            <div className="rounded" id="productCard">
-              <img className="productImg" src={product} alt="product1" />
-              {productdata.map(({ id2, productname2, productinfo2 }) => (
-                <div className="p-2">
-                  <h3 key={id2}>{productname2}</h3>
-                  <p>{productinfo2}</p>
-                </div>
-              ))}
-            </div>
-          </a>
-
-          <a href="/PS25">
-            <div className="rounded" id="productCard">
-              <img className="productImg" src={product} alt="product1" />
-              <div className="p-2">
-                <h3>PS25 Foot Type</h3>
-                <p>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-              </div>
-            </div>
-          </a>
-          <a href="/PS25">
-            <div className="rounded" id="productCard">
-              <img className="productImg" src={product} alt="product1" />
-              <div className="p-2">
-                <h3>PS25 Flange Type</h3>
-                <p>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card.
-                </p>
-              </div>
-            </div>
-          </a>
-          <a href="/PS25">
-            <div className="rounded" id="productCard">
-              <img className="productImg" src={product} alt="product1" />
-              <div className="p-2">
-                <h3>PS30 Foot Type</h3>
-                <p>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-              </div>
-            </div>
-          </a>
-          <a href="/PS25">
-            <div className="rounded" id="productCard">
-              <img className="productImg" src={product} alt="product1" />
-              <div className="p-2">
-                <h3>PS30 Flange Type</h3>
-                <p>
-                  Some quick example text to build on the card title and make up
-                  the bulkof the card.
-                </p>
-              </div>
-            </div>
-          </a>
-          <a href="/PS25">
-            <div className="rounded" id="productCard">
-              <img className="productImg" src={product} alt="product1" />
-              <div className="p-2">
-                <h3>PS35 Foot Type</h3>
-                <p>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-              </div>
-            </div>
-          </a>
-          <a href="/PS25">
-            <div className="rounded" id="productCard">
-              <img className="productImg" src={product} alt="product1" />
-              <div className="p-2">
-                <h3>PS35 Flange Type</h3>
-                <p>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card.
-                </p>
-              </div>
-            </div>
-          </a> */}
         </div>
       </Container>
     </div>
--- src/functions/routes.js
+++ src/functions/routes.js
@@ -4,7 +4,7 @@ import App from "../pages/App";
 import Footer from "../components/Shared/Footer/footer";
 import Header from "../components/Shared/Header/header";
 import Agriculture from "../pages/Agriculture/Agriculture";
-import PS25 from "../pages/PS25/PS25";
+import Product from "../pages/Products/products";
 import Media from "../components/Shared/Social_Media_Bar/media";
 import ContactBar from "../components/Shared/Social_Media_Bar/contactBar";
 import InfoPage from "../pages/Info/Info";
@@ -18,7 +18,7 @@ const ConfigRoutes = () => {
         <Route path="/blogs" element={<App />}></Route>
         <Route path="/contact" element={<App />}></Route>
         <Route path="/agriculture" element={<Agriculture />}></Route>
-        <Route path="/PS25" element={<PS25 />}></Route>
+        <Route path="/product/:id" element={<Product />}></Route>
         <Route path="/info" element={<InfoPage />}></Route>
       </Routes>
       <Media></Media>
